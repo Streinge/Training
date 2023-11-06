b6fca66156841b5147d612045c277cfde8a7f8cc
Streinge <olesof@mail.ru>
2023-11-06 23:00:56 +0500

    6/11 домашний 23:00

---
+++ text23.py
@@ -0,0 +1,7 @@
+tree = 0x0000024A8B68BFD0
+tree.__dict__ = {'key': None, 'parent': None, 'descendant': [], 'cur': <0x0000020C9A40BFD0>}
+tree.add('1st')
+    add(self=0x0000027DDEC4BFD0, key='1st')
+    tmp = new_obj(self.cur=<0x0000020C9A40BFD0>, key='1st', parent=<0x0000020C9A40BFD0>)
+    tmp = 0x0000027DDEC4BCD0
+    tmp.__dict__ = {'key': '1st', 'parent': <0x00000194C6BCBFD0>, 'descendant': [], 'cur': <0x00000194C6BCBCD0>}
