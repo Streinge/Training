3a2081dc4951837e7408897b7dbd491e6f37b46d
Streinge <olesof@mail.ru>
2023-11-08 00:12:23 +0500

    сделал базу испытания 3 домашний комп 8/11 0:12

--- TreeBuilder.py
+++ TreeBuilder.py
@@ -1,21 +1,22 @@
 class TreeBuilder:
-    def __init__(self, key=None):
+    def __init__(self, key=None, parent=None):
         self.key = key
         self.descendant = []
         self.current = self
+        self.parent = parent
 
     def add(self, key):
-        tmp = self.__class__(key)
-        self.descendant.append(tmp)
-        self.current = tmp
+        tmp = self.current.__class__(key, parent=self.current)
+        self.current.descendant.append(tmp)
         return tmp
 
     def __enter__(self):
         print('self.current ', self.current)
-        self.current = self.descendant[0]
+        if self.current.descendant[0]:
+            self.current = self.current.descendant[0]
 
     def __exit__(self, exc_type, exc_value, traceback):
-        return self
+        self.current = self.current.parent
 
     def _structure(self, list_full):
         if self.descendant:
@@ -35,16 +36,17 @@ class TreeBuilder:
     def structure(self):
         return self._structure([])
 
+
 tree = TreeBuilder()
 print(tree.structure)
-_1st = tree.add('1st')
+tree.add('1st')
 print(tree.structure)
-"""with tree:
-    _2nd = tree.add('2nd')
+with tree:
+    tree.add('2nd')
     with tree:
-        _3st = tree.add('3rd')"""
+        tree.add('3rd')
+    tree.add('4th')
+    with tree:
+        pass
 print(tree.structure)
 print(tree.__dict__)
-"""print(_1st.__dict__)
-print(_2nd.__dict__)
-print(_3st.__dict__)"""
