f6d8dd49551bc102d6c74848b52dd0d812363e7a
Streinge <olesof@mail.ru>
2023-11-13 14:17:20 +0500

    домашний комп 13/11 14:17

--- Binary_tree_search.py
+++ Binary_tree_search.py
@@ -8,28 +8,21 @@ class Node:
         x = self.__class__.__name__
         return f"{x}({self.key!r}, {self.left}, {self.right})"
 
-    def search(self, key, target=None):
-        print(key)
+    def search(self, key):
         if self.key == key:
-            print('значение нашлось')
-            target = self
-            print('target ', target)
             return self
         else:
-            if self.left is not None:
-                print('заходил влево')
-                print('self.left.key ', self.left.key)
-                print('target ', target)
-                self.left.search(key, target)
-            if self.right is not None:
-                print('заходил вправо')
-                print('self.right.key ', self.right.key)
-                print('target ', target)
-                self.right.search(key, target)
-        return target
+            if key < self.key:
+                if self.left is not None:
+                    return self.left.search(key)
+            if key >= self.key:
+                if self.right is not None:
+                    return self.right.search(key)
+                
 
 
-node5 = Node(5)
+
+""" node5 = Node(5)
 node22 = Node(22, left=Node(20))
 tree = Node(
     9,
@@ -49,4 +42,34 @@ tree = Node(
 )
 
 print(tree)
-print(tree.search(6))
+print(tree.search(6).key)
+print(tree.search(6).right.key)
+print(tree.search(5) is node5)
+print(tree.left.left.key)
+single_node = Node(1)
+print(single_node.key)
+print(single_node.left)
+print(single_node.right) """
+node5 = Node(5)
+node22 = Node(22, right=Node(20))
+tree = Node(
+    9,
+    left=Node(
+        4,
+        left=Node(
+            6,
+            left=Node(5),
+            right=Node(7),
+        ),
+        right=Node(3),
+    ),
+    right=Node(
+        17,
+        right=Node(22),
+    ),
+)
+print(tree.search(5) is None)
+print(tree.search(7) is None)
+print(tree.search(6) is None)
+print(tree.search(4).key == 4)
+print(tree.search(22).key == 22)
